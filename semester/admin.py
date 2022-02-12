6528c7b527c9cfce0b93a09aed48b91d4447563c
NobinKhan <nobin118960@gmail.com>
2022-02-13 03:45:33 +0600

    Course and Semester Result added

--- semester/admin.py
+++ semester/admin.py
@@ -1,8 +1,10 @@
 from ast import Assign
 from django.contrib import admin
-from .models import Attendance, AssignCourse, Period, Shedule
+from .models import Attendance, AssignCourse, CourseResult, Period, SemResult, Shedule
 # Register your models here.
 admin.site.register(Period)
 admin.site.register(Shedule)
 admin.site.register(AssignCourse)
 admin.site.register(Attendance)
+admin.site.register(CourseResult)
+admin.site.register(SemResult)
