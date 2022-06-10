3e83f27b59643d3a60756e7aed3a2c1a07a13bef
NobinKhan <nobin118960@gmail.com>
2022-06-10 18:20:55 +0600

    username validator added

--- core/models.py
+++ core/models.py
@@ -106,7 +106,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         unique=True,
         help_text=_(
             'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        # validators=[username_validator],
+        validators=[username_validator],
         error_messages={
             'unique': _("A user with that username already exists."),
         },
