bb01fc21a98481d5d27edf2b0ae560957b5ec9e7
Md. Nazrul Islam Khan <nazrulkhan.dk@gmail.com>
2022-07-13 10:53:54 +0600

    gender choices field validation added

--- core/mutation.py
+++ core/mutation.py
@@ -87,29 +87,31 @@ class CreateStudent(Mutation):
         data = CreateStudentInput()
     student = Field(UserType)
     def mutate(root, info, data=None):
-        sessionInstance = get_object_or_None(Session, pk=data.joiningSessionID)
-        programInstance = get_object_or_None(Program, pk=data.programID)
-        semesterInstance = get_object_or_None(Semester, pk=data.joiningSemesterID)
-        designationInstance = get_object_or_None(Designation, pk=data.designationID)
-        if sessionInstance and programInstance and semesterInstance and designationInstance:
-            newStudent = User(
-                email = data.email,
-                date_of_birth = data.dateOfBirth,
-                gender = data.gender,
-                joinedSession= sessionInstance,
-                isStudent=True,
-                studentAddmissionType=data.studentType,
-                program=programInstance,
-                joinedSemester=semesterInstance,
-                designation=designationInstance,
-            )
-            newStudent.set_password(data.password)
-            newStudent.save()
-            userGroup = get_object_or_None(Group, pk=data.groupID)
-            grp = Group.objects.filter(user = newStudent)
-            if userGroup and not grp:
-                newStudent.groups.add(userGroup)
-            return CreateStudent(student=newStudent)
+        if data.studentType in [d[0] for d in User.typeChoices] and data.gender in [d[0] for d in User.genderChoices]:
+            sessionInstance = get_object_or_None(Session, pk=data.joiningSessionID)
+            programInstance = get_object_or_None(Program, pk=data.programID)
+            semesterInstance = get_object_or_None(Semester, pk=data.joiningSemesterID)
+            designationInstance = get_object_or_None(Designation, pk=data.designationID)
+            if sessionInstance and programInstance and semesterInstance and designationInstance:
+                newStudent = User(
+                    email = data.email,
+                    date_of_birth = data.dateOfBirth,
+                    gender = data.gender,
+                    joinedSession= sessionInstance,
+                    isStudent=True,
+                    studentAddmissionType=data.studentType,
+                    program=programInstance,
+                    joinedSemester=semesterInstance,
+                    designation=designationInstance,
+                )
+                newStudent.set_password(data.password)
+                newStudent.save()
+                userGroup = get_object_or_None(Group, pk=data.groupID)
+                grp = Group.objects.filter(user = newStudent)
+                if userGroup and not grp:
+                    newStudent.groups.add(userGroup)
+                return CreateStudent(student=newStudent)
+        return None
 
 
 class CreateTeacherInput(InputObjectType):
@@ -128,26 +130,28 @@ class CreateTeacher(Mutation):
         data = CreateTeacherInput()
     teacher = Field(UserType)
     def mutate(root, info, data=None):
-        sessionInstance = get_object_or_None(Session, pk=data.joiningSessionID)
-        departmentInstance = get_object_or_None(Department, pk=data.departmentID)
-        designationInstance = get_object_or_None(Designation, pk=data.designationID)
-        if sessionInstance and departmentInstance and designationInstance:
-            newTeacher = User(
-                email = data.email,
-                date_of_birth = data.dateOfBirth,
-                gender = data.gender,
-                joinedSession= sessionInstance,
-                isTeacher=True,
-                department=departmentInstance,
-                designation=designationInstance,
-            )
-            newTeacher.set_password(data.password)
-            newTeacher.save()
-            userGroup = get_object_or_None(Group, pk=data.groupID)
-            grp = Group.objects.filter(user = newTeacher)
-            if userGroup and not grp:
-                newTeacher.groups.add(userGroup)
-            return CreateTeacher(teacher=newTeacher)
+        if data.gender in [d[0] for d in User.genderChoices]:
+            sessionInstance = get_object_or_None(Session, pk=data.joiningSessionID)
+            departmentInstance = get_object_or_None(Department, pk=data.departmentID)
+            designationInstance = get_object_or_None(Designation, pk=data.designationID)
+            if sessionInstance and departmentInstance and designationInstance:
+                newTeacher = User(
+                    email = data.email,
+                    date_of_birth = data.dateOfBirth,
+                    gender = data.gender,
+                    joinedSession= sessionInstance,
+                    isTeacher=True,
+                    department=departmentInstance,
+                    designation=designationInstance,
+                )
+                newTeacher.set_password(data.password)
+                newTeacher.save()
+                userGroup = get_object_or_None(Group, pk=data.groupID)
+                grp = Group.objects.filter(user = newTeacher)
+                if userGroup and not grp:
+                    newTeacher.groups.add(userGroup)
+                return CreateTeacher(teacher=newTeacher)
+        return None
 
 
 class CreateStaffInput(InputObjectType):
@@ -165,24 +169,26 @@ class CreateStaff(Mutation):
         data = CreateStaffInput()
     staff = Field(UserType)
     def mutate(root, info, data=None):
-        sessionInstance = get_object_or_None(Session, pk=data.joiningSessionID)
-        designationInstance = get_object_or_None(Designation, pk=data.designationID)
-        if sessionInstance and designationInstance:
-            newStaff = User(
-                email = data.email,
-                date_of_birth = data.dateOfBirth,
-                gender = data.gender,
-                joinedSession= sessionInstance,
-                is_staff=True,
-                designation=designationInstance,
-            )
-            newStaff.set_password(data.password)
-            newStaff.save()
-            userGroup = get_object_or_None(Group, pk=data.groupID)
-            grp = Group.objects.filter(user = newStaff)
-            if userGroup and not grp:
-                newStaff.groups.add(userGroup)
-            return CreateStaff(staff=newStaff)
+        if data.gender in [d[0] for d in User.genderChoices]:
+            sessionInstance = get_object_or_None(Session, pk=data.joiningSessionID)
+            designationInstance = get_object_or_None(Designation, pk=data.designationID)
+            if sessionInstance and designationInstance:
+                newStaff = User(
+                    email = data.email,
+                    date_of_birth = data.dateOfBirth,
+                    gender = data.gender,
+                    joinedSession= sessionInstance,
+                    is_staff=True,
+                    designation=designationInstance,
+                )
+                newStaff.set_password(data.password)
+                newStaff.save()
+                userGroup = get_object_or_None(Group, pk=data.groupID)
+                grp = Group.objects.filter(user = newStaff)
+                if userGroup and not grp:
+                    newStaff.groups.add(userGroup)
+                return CreateStaff(staff=newStaff)
+        return None
 
 
 class CreateProfileInput(InputObjectType):
@@ -228,6 +234,51 @@ class CreateProfile(Mutation):
             )
             newProfile.save()
             return CreateProfile(profile=newProfile)
+
+
+class CreatePreviousEducationInput(InputObjectType):
+    userID = ID(required=True)
+    firstName = String(required=True)
+    lastName = String(required=True)
+    permanentAddress = String(required=True)
+    presentAddress = String(required=True)
+    mobilePhone = String(required=True)
+    mobilePhone2 = String()
+    nid = Float()
+    birthCertNumber = Float(required=True)
+    nationalityID = ID(required=True)
+    fatherName = String(required=True)
+    motherName = String(required=True)
+    photo = String(required=True)
+
+
+class CreatePreviousEducation(Mutation):
+    class Arguments:
+        data = CreatePreviousEducationInput()
+    profile = Field(PreviousEducationType)
+    def mutate(root, info, data=None):
+        PreviousEducation.phone_regex(data.mobilePhone)
+        PreviousEducation.phone_regex(data.mobilePhone2)
+        userInstance = get_object_or_None(User, pk=data.userID)
+        nationalityInstance = get_object_or_None(Nationality, pk=data.nationalityID)
+        if userInstance and nationalityInstance:
+            newPreviousEducation = PreviousEducation(
+                user=userInstance,
+                first_name = data.firstName,
+                last_name = data.lastName,
+                permanentAddress = data.permanentAddress,
+                presentAddress = data.presentAddress,
+                mobilePhone = data.mobilePhone,
+                mobilePhone2 = data.mobilePhone2,
+                nid = data.nid,
+                birthCertNumber = data.birthCertNumber,
+                nationality = nationalityInstance,
+                fatherName = data.fatherName,
+                motherName = data.motherName,
+                photo = data.photo,
+            )
+            newPreviousEducation.save()
+            return CreatePreviousEducation(profile=newPreviousEducation)
 
 
 class UpdateUserInput(InputObjectType):
@@ -335,20 +386,6 @@ class Mutation(ObjectType):
 #   token_auth = graphql_jwt.ObtainJSONWebToken.Field()
 #   verify_token = graphql_jwt.Verify.Field()
 #   refresh_token = graphql_jwt.Refresh.Field()
-
-        # username = data.userName,
-        # first_name = data.firstName,
-        # last_name = data.lastName,
-        # permanentAddress = data.permanentAddress,
-        # presentAddress = data.presentAddress,
-        # mobilePhone = data.mobilePhone,
-        # mobilePhone2 = data.mobilePhone2,
-        # nid = data.nid,
-        # birthCertNumber = data.birthCertNumber,
-        # nationality = nationalityInstance,
-        # fatherName = data.fatherName,
-        # motherName = data.motherName,
-        # photo = data.photo,
 
 
 
